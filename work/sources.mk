2f1da122933d7aa0469277c20f548d85a0c44256
Masaki Hara <ackie.h.gmai@gmail.com>
2014-12-02 19:55:25 +0900

    refactor Makefile : use ghdl -m

---
+++ work/sources.mk
@@ -0,0 +1,11 @@
+SOURCES = \
+	serial.vhd kakeudon.vhd memory_controller.vhd register_file.vhd \
+	reservation_station.vhd reorder_buffer.vhd \
+	load_store_buffer.vhd \
+	fp_adder.vhd fp_multiplier.vhd fp_comparator.vhd fp_others.vhd \
+	alu.vhd io_rs232c.vhd core.vhd cpu.vhd \
+	sramsim.vhd cpu_tb.vhd \
+	../fpu/VHDL/fadd.vhd ../fpu/VHDL/fdiv.vhd ../fpu/VHDL/finv.vhd \
+	../fpu/VHDL/fmul_stage1.vhd ../fpu/VHDL/fmul_stage2.vhd \
+	../fpu/VHDL/fmul.vhd ../fpu/VHDL/fsqrt.vhd ../fpu/VHDL/ftoi.vhd \
+	../fpu/VHDL/itof.vhd ../fpu/VHDL/kakeudon_fpu.vhd
